179487d88eeed17dec0241dd4bb50e83b4e4db67
MadushaHero <madushalakmal@gmail.com>
2022-05-14 10:15:22 +0530

    DataBase And Submition Form Created

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.String(200),nullable=False)
+    Completed = db.Column(db.Integer,default=0)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__ (self):
+        return '(Task %r) ' % self.id
+
+
+@app.route('/', methods=['POST','GET'])
 
 def index():
-    return render_template('index.html')
-
+    if request.method == 'POST':
+        pass
+        return('Hello')
+    
+    else:
+        return render_template('index.html')
+        
+   
 if __name__ == "__main__":
     app.run(debug=True)
 
